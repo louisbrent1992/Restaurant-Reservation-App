654ed5cb16fd99ce386e5f7cb1cf1264b9df4a5e
Louis <louisbrent1992@gmail.com>
2024-03-25 08:05:38 -0700

    production ready server updates

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -7,28 +7,15 @@
 require("dotenv").config();
 const path = require("path");
 
-const {
-	DATABASE_URL_PRODUCTION,
-	DATABASE_URL_DEVELOPMENT,
-	DB_HOST,
-	DB_USER,
-	DB_NAME,
-	DB_PASSWORD,
-	DB_SSL,
-	DB_PORT,
-	DEBUG,
-} = process.env;
+const { CLIENT_URL_PRODUCTION, CLIENT_URL_DEVELOPMENT, DB_SSL, DEBUG } =
+	process.env;
 
 module.exports = {
 	development: {
 		client: "pg",
 		connection: {
-			connectionString: DATABASE_URL_DEVELOPMENT,
-			host: DB_HOST,
-			port: DB_PORT,
-			user: DB_USER,
-			database: DB_NAME,
-			password: DB_PASSWORD,
+			connectionString: CLIENT_URL_DEVELOPMENT,
+
 			ssl: DB_SSL ? { rejectUnauthorized: false } : true,
 
 			debug: !!DEBUG,
@@ -45,7 +32,12 @@ module.exports = {
 	production: {
 		client: "pg",
 		pool: { min: 1, max: 5 },
-		connection: DATABASE_URL_PRODUCTION,
+		connection: {
+			CLIENT_URL_PRODUCTION,
+			ssl: DB_SSL ? { rejectUnauthorized: false } : true,
+			debug: !!DEBUG,
+		},
+
 		migrations: {
 			directory: path.join(__dirname, "src", "db", "migrations"),
 		},
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,23 @@
-const { PORT = 5000 } = process.env;
+const {
+	NODE_ENV,
+	CLIENT_URL_PRODUCTION,
+	CLIENT_URL_DEVELOPMENT,
+	PORT = 5000,
+} = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
+const cors = require("cors");
+
+app.use(
+	cors({
+		allowedOrigins: [
+			NODE_ENV === "production"
+				? CLIENT_URL_PRODUCTION
+				: CLIENT_URL_DEVELOPMENT,
+		],
+	})
+);
 
 knex.migrate
 	.latest()
